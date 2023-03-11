018c30c1708c1fe64225fd7527aa249321241013
AbdullahKhan1357 <abdkhan13575@gmail.com>
2023-03-11 19:39:03 +0500

    First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed;
+    private Rigidbody2D rb2d;
+    private Vector2 playerDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionY = Input.GetAxisRaw("Vertical");
+        playerDirection = new Vector2(0, directionY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(0, playerDirection.y * playerSpeed);
+    }
+}
